3cdf24612f90547126505dc3819f7e93b4441e4e
Leonard Dixon <ldixon@compunetix.com>
2020-05-08 15:47:18 -0400

    Latest changes

--- server.js
+++ server.js
@@ -9,36 +9,40 @@ const port = new SerialPort('COM1', {
 
 const parser = new Readline()
 port.pipe(parser)
-parser.on('data', console.log)
-
 
 
 const server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
+    //console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
+
 const wss = new WebSocket.Server({ server });
 
 const lineByLine = require('n-readlines');
 const liner = new lineByLine('nellcor.txt');
 
 let line;
-let lineNumber = 0;
-
-
 
 wss.on('connection', function connection(ws) {
+    parser.on('data', function(data) {
+        ws.send(data)
+    })
+    
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
     while (line = liner.next()) {
-        console.log('Line ' + lineNumber + ': ' + line.toString('ascii'));
-        ws.send(line);
-        lineNumber++;
+        //console.log(line.toString('ascii'));
+        var data = line.toString().split(" ");
+        if(data[0] != "N-550" && data[0] != "TIME" && data[0] != "") {
+            console.log(data[0]);
+            var time = data[2];
+        }
+        ws.send(line.toString('ascii') + "<br />");
     }
     
-    console.log('end of line reached');
+    //console.log('end of line reached');
 });
 
 server.listen(8080);
